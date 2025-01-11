2c20076f6c7a9685f22a77317ac6c7881c356141
BlarQ <collinsogunlala@gmail.com>
2025-01-11 15:45:53 +0100

    feat: fixed home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,30 +34,30 @@ export default function Home() {
   }
   
   return (
-    <div className="flex h-[100dvh] items-center justify-between flex-col py-10 px-8">
-      <div className="flex items-center justify-center flex-col bg-white">
+    <div className="flex h-[100dvh] items-center justify-between flex-col py-10 px-4 md:px-8">
+  <div className="flex items-center justify-center flex-col bg-white">
 
-        <Image className="sm:w-60 sm:h-60" src={'/shoppingSale1.svg'} width={500} height={500} alt="Welcome Image" />
+    <Image className="w-80 h-80 sm:w-60 sm:h-60" src={'/shoppingSale1.svg'} width={500} height={500} alt="Welcome Image" />
 
 
-        <div className="text-center py-6 px-4 text-[#000000E5]">
-          <h1 className="text-4xl font-bold">Welcome</h1>
-          <p className="text-[14px] font-normal">The safest platfrom to shop from social media vendors</p>
-        </div>
-
-        <div className="bg-[#FFEAFA] p-4 border border-[#8A226F] w-full rounded-xl">
-          {
-            homeInfo.map((info) => (
-              <div key={info.id} className="flex items-center justify-start py-1">
-                <Image className="mr-4 size-6" src={info.image} width={50} height={50} alt={info.description} />
-                <h2 className="text-[14px] font-medium">{info.description}</h2>
-              </div>
-            ))
-          }
-        </div>
-      </div>
-      
-      <Button onClick={handleClick}/>
+    <div className="text-center py-6 px-4 text-[#000000E5]">
+      <h1 className="text-3xl sm:text-4xl font-bold">Welcome</h1>
+      <p className="text-[12px] sm:text-[14px] font-normal">The safest platfrom to shop from social media vendors</p>
     </div>
+
+    <div className="bg-[#FFEAFA] p-4 border border-[#8A226F] w-full rounded-xl">
+      {
+        homeInfo.map((info) => (
+          <div key={info.id} className="flex items-center justify-start py-1">
+            <Image className="mr-4 size-6" src={info.image} width={40} height={40} alt={info.description} />
+            <h2 className="text-[12px] sm:text-[14px] font-medium">{info.description}</h2>
+          </div>
+        ))
+      }
+    </div>
+  </div>
+  
+  <Button onClick={handleClick} className="md:mt-10"/>
+</div>
   );
 }
